f1a688ba297e75956175cc8481eadc55bff44888
Anais Fontalvo <anaisfontalvocarrillo@gmail.com>
2026-02-25 21:40:33 -0500

    subiendo mis archivos

---
+++ Calculadora(desarrollo)/funciones.js
@@ -0,0 +1,74 @@
+function uno()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"1";
+}
+
+function dos()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"2";
+}
+
+function tres()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"3";
+}
+
+function cuatro()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"4";
+}
+
+function cinco()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"5";
+}
+
+function seis()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"6";
+}
+
+function siete()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"7";
+}
+
+function ocho()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"8";
+}
+
+function nueve()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"9";
+}
+
+function mas()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"+";
+}
+
+function resta()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"-";
+}
+
+function div()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"/";
+}
+
+function mul()
+{
+    document.getElementById("txtpantalla").value=document.getElementById("txtpantalla").value+"*";
+}
+
+function resultado()
+{
+    document.getElementById("txtpantalla").value=eval(document.getElementById("txtpantalla").value)
+}
+
+document.getElementById("txtpantalla").value=""
+function borrar(){
+    document.getElementById("txtpantalla").value=""
+}
